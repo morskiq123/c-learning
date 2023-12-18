0cda5230dd72700b5bd6590facfa7bf2d70bae7e
morskiq123 <ayotov99@gmail.com>
2023-12-18 12:34:13 +0200

    1.5.3.

--- hello.c
+++ hello.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
-
+#define IN 1
+#define OUT 0
 
 main() {
 
-    long nc;
+    int c, nw, nc, nl, state;
 
-    nc = 0;
-    while (getchar() != EOF)
-        ++nc;
+    state = OUT;
+    nw = nc = nl = 0;
+
+    while ((c == getchar()) != EOF){
+        if (c == '\n')
+            ++nl;
+        if (c == ' ' || c == '\t' || c == '\n'){
+            state = OUT;
+        }
+        else if (state == OUT){
+            state = IN;
+            ++nw;
+        }
+    }
     
-    printf("%ld", nc);
-
+    printf("%d %d %d\n", nl, nw, nc); 
 }
